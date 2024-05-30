0c36e174b75aa6c19e9cb7b044df418a271bcc8e
Reginald Amuzu <reginaldrock219@gmail.com>
2024-05-30 03:26:08 +0100

    login & register pages fully done

---
+++ frontend/src/components/RegisterForm.jsx
@@ -0,0 +1,66 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import api from "../api";
+import { useNavigate } from "react-router-dom";
+import "../styles/Form.css"
+import LoadingIndicator from "./LoadingIndicator";
+
+function Form({ route }) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        setLoading(true);
+        e.preventDefault();
+
+        try {
+            const res = await api.post(route, { username, password, email })
+            if (res) {
+                alert("Success")
+                navigate("/login")
+            } else {
+                navigate("/login")
+            }
+        } catch (error) {
+            alert(error)
+        } finally {
+            setLoading(false)
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="form-container">
+            <h1>Register</h1>
+            <input
+                className="form-input"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+            />
+            <input
+                className="form-input"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+            />
+            <input
+                className="form-input"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+            />
+            {loading && <LoadingIndicator />}
+            <button className="form-button" type="submit">
+                Register
+            </button>
+        </form>
+    );
+}
+
+export default Form
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+function Home() {
+    return <div>
+        <h1>Home</h1>
+        <p>The Home Page</p>
+    </div>
+}
+
+export default Home;
